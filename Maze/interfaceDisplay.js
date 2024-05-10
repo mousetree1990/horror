083e8cbd082fb3a2602d41f4556b4478987fd806
mousetree1990 <140615137+mousetree1990@users.noreply.github.com>
2024-05-09 21:50:36 -0500

    Add files via upload

---
+++ Maze/interfaceDisplay.js
@@ -0,0 +1,176 @@
+
+// inventory button 1 display
+function ib1(){
+    let x = document.getElementById("potions");
+    let checkWeaponVis = document.getElementById("weapons");
+    let checkArmorVis = document.getElementById("armor");
+    let checkKeyVis = document.getElementById("keyItems");
+    let checkOptionVis = document.getElementById("options");
+    let checkInventoryVis = document.getElementById("inventory");
+    if (x.style.display === "flex"){
+       x.style.display = "none";
+    } else {
+       x.style.display = "flex";
+    }
+    if (checkWeaponVis.style.display === "flex"){
+       checkWeaponVis.style.display = "none";
+    }
+    if (checkArmorVis.style.display === "flex"){
+       checkArmorVis.style.display = "none";
+    }
+    if (checkKeyVis.style.display === "flex"){
+       checkKeyVis.style.display = "none";
+    }
+    if (checkOptionVis.style.display === "flex"){
+       checkOptionVis.style.display = "none";
+    }
+    if (checkInventoryVis.style.display === "none"){
+        checkInventoryVis.style.display = "flex";
+    } else {
+        checkInventoryVis.style.display = "flex";
+    }
+    if (x.style.display === "none"){
+        checkInventoryVis.style.display = "none";
+    }
+    }
+   
+    // inventory button 2 display
+    function ib2(){
+        let x = document.getElementById("weapons");
+        let checkPotionVis = document.getElementById("potions")
+        let checkArmorVis = document.getElementById("armor");
+        let checkKeyVis = document.getElementById("keyItems");
+        let checkOptionVis = document.getElementById("options");
+        let checkInventoryVis = document.getElementById("inventory");
+        if (x.style.display === "flex"){
+          x.style.display = "none";
+        } else {
+          x.style.display = "flex";
+        }
+        if (checkArmorVis.style.display === "flex"){
+           checkArmorVis.style.display = "none";
+        }
+        if (checkKeyVis.style.display === "flex"){
+           checkKeyVis.style.display = "none";
+        }
+        if (checkOptionVis.style.display === "flex"){
+           checkOptionVis.style.display = "none";
+        }
+        if (checkPotionVis.style.display === "flex"){
+           checkPotionVis.style.display = "none";
+        }
+        if (checkInventoryVis.style.display === "none"){
+            checkInventoryVis.style.display = "flex";
+        } else {
+            checkInventoryVis.style.display = "flex";
+        }
+        if (x.style.display === "none"){
+            checkInventoryVis.style.display = "none";
+        }
+       
+        }
+   
+        // inventory button 3 display
+    function ib3(){
+        let x = document.getElementById("armor");
+        let checkWeaponVis = document.getElementById("weapons");
+        let checkPotionVis = document.getElementById("potions");
+        let checkKeyVis = document.getElementById("keyItems");
+        let checkOptionVis = document.getElementById("options");
+        let checkInventoryVis = document.getElementById("inventory");
+        if (x.style.display === "flex"){
+            x.style.display = "none";
+        } else {
+            x.style.display = "flex";   
+        }
+        if (checkKeyVis.style.display === "flex"){
+           checkKeyVis.style.display = "none";
+        }
+        if (checkOptionVis.style.display === "flex"){
+           checkOptionVis.style.display = "none";
+        }
+        if (checkPotionVis.style.display === "flex"){
+           checkPotionVis.style.display = "none";
+        }
+        if (checkWeaponVis.style.display === "flex"){
+           checkWeaponVis.style.display = "none";
+        }
+        if (checkInventoryVis.style.display === "none"){
+            checkInventoryVis.style.display = "flex";
+        } else {
+            checkInventoryVis.style.display = "flex";
+        }
+        if (x.style.display === "none"){
+            checkInventoryVis.style.display = "none";
+        }
+        }
+   
+        // inventory button 4 display
+    function ib4(){
+        let x = document.getElementById("keyItems");
+        let checkWeaponVis = document.getElementById("weapons");
+        let checkPotionVis = document.getElementById("potions");
+        let checkOptionVis = document.getElementById("options");
+        let checkArmorVis = document.getElementById("armor");
+        let checkInventoryVis = document.getElementById("inventory");
+        if (x.style.display === "flex"){
+        x.style.display = "none";
+        } else {
+        x.style.display = "flex";        
+        }
+        if (checkOptionVis.style.display === "flex"){
+           checkOptionVis.style.display = "none";
+        }
+        if (checkPotionVis.style.display === "flex"){
+           checkPotionVis.style.display = "none";
+        }
+        if (checkWeaponVis.style.display === "flex"){
+           checkWeaponVis.style.display = "none";
+        }
+        if (checkArmorVis.style.display === "flex"){
+           checkArmorVis.style.display = "none";
+        }
+        if (checkInventoryVis.style.display === "none"){
+            checkInventoryVis.style.display = "flex";
+        } else {
+            checkInventoryVis.style.display = "flex";
+        }
+        if (x.style.display === "none"){
+            checkInventoryVis.style.display = "none";
+        }
+    }
+   
+       // inventory button 5 display
+    function ib5(){
+        let x = document.getElementById("options");
+        let checkWeaponVis = document.getElementById("weapons");
+        let checkPotionVis = document.getElementById("potions");
+        let checkArmorVis = document.getElementById("armor");
+        let checkKeyVis = document.getElementById("keyItems");
+        let checkInventoryVis = document.getElementById("inventory");
+        if (x.style.display === "flex"){
+        x.style.display = "none";
+        } else {
+        x.style.display = "flex";             
+        }
+        if (checkPotionVis.style.display === "flex"){
+           checkPotionVis.style.display = "none";
+        }
+        if (checkWeaponVis.style.display === "flex"){
+           checkWeaponVis.style.display = "none";
+        }
+        if (checkArmorVis.style.display === "flex"){
+           checkArmorVis.style.display = "none";
+        }
+        if (checkKeyVis.style.display === "flex"){
+           checkKeyVis.style.display = "none";
+        }
+        if (checkInventoryVis.style.display === "none"){
+            checkInventoryVis.style.display = "flex";
+        } else {
+            checkInventoryVis.style.display = "flex";
+        }
+        if (x.style.display === "none"){
+            checkInventoryVis.style.display = "none";
+        }
+    }
---
+++ Maze/storyFrame.js
@@ -0,0 +1,13 @@
+
+let Gate1 = 1;
+
+
+
+
+function StoryButton1(){
+switch(Gate1){
+    case 1:
+    alert('switch test 1')
+    break;
+}
+}
